fa7fa0b066e1ba7e0682205cc57f580d5fea9a65
austenppm <austenppm12345@gmail.com>
2023-12-28 15:05:07 +0900

    year

--- 2.6.py
+++ 2.6.py
@@ -1,12 +1,13 @@
-# def fukuri(ganpon, kinri, year):
-#     chokin = ganpon
-#     for year in range(1, year+1) :
-#         chokin = chokin + chokin * kinri
-#         print(year, chokin)
-#     return chokin
+def fukuri(ganpon, kinri, year):
+    chokin = ganpon
+    for current_year in range(1, year+1) :
+        chokin = chokin + chokin * kinri
+        print(current_year, chokin)
+        print(year)
+    return chokin
 
-# A = fukuri(100000, 0.01, 10)
-# B = fukuri(100000, 0.02, 10)
+A = fukuri(100000, 0.01, 10)
+B = fukuri(100000, 0.02, 10)
 
 # def fukuri(ganpon, kinri1, kinri2, year):
 #     chokin1 = ganpon
@@ -26,21 +27,21 @@
 # print(chokin)
 # print(year)
 
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-def sin_sum(x, n):
-    sum = 0
-    for i in range(1, n+1):
-        sum += np.sin(i*x) / i
-    return sum
+# def sin_sum(x, n):
+#     sum = 0
+#     for i in range(1, n+1):
+#         sum += np.sin(i*x) / i
+#     return sum
 
-x = np.arange(0, 2*np.pi, 0.1)
-y1 = sin_sum(x, 3)
-y2 = sin_sum(x, 3)
-plt.plot(x, y1)
-plt.plot(x, y2)
-plt.show()
+# x = np.arange(0, 2*np.pi, 0.1)
+# y1 = sin_sum(x, 3)
+# y2 = sin_sum(x, 3)
+# plt.plot(x, y1)
+# plt.plot(x, y2)
+# plt.show()
 
 # import numpy as np
 # import matplotlib.pyplot as plt
