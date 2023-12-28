40a84555f22d64f0f2cf6d8ec6e323dd1523b9a9
austenppm <austenppm12345@gmail.com>
2023-12-28 10:31:52 +0900

    morechanges

--- 2.6.py
+++ 2.6.py
@@ -26,32 +26,32 @@
 # print(chokin)
 # print(year)
 
-# import numpy as np
-# import matplotlib.pyplot as plt
-
-# def sin_sum(x, n):
-#     sum = 0
-#     for i in range(1, n+1):
-#         sum += np.sin(i*x) / i
-#     return sum
-
-# x = np.arange(0, 2*np.pi, 0.1)
-# y1 = sin_sum(x, 50)
-# y2 = sin_sum(x, 3)
-# plt.plot(x, y1)
-# plt.plot(x, y2)
-# plt.show()
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-def sin_sum24(x, n):
-    sum = 0 
-    for i in range (1, n+1):
-        sum = sum + np.sin((2*i-1)*x) / (2*i-1)
+def sin_sum(x, n):
+    sum = 0
+    for i in range(1, n+1):
+        sum += np.sin(i*x) / i
     return sum
 
 x = np.arange(0, 2*np.pi, 0.1)
-y1 = sin_sum24(x, 50)
+y1 = sin_sum(x, 3)
+y2 = sin_sum(x, 3)
 plt.plot(x, y1)
+plt.plot(x, y2)
 plt.show()
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# def sin_sum24(x, n):
+#     sum = 0 
+#     for i in range (1, n+1):
+#         sum = sum + np.sin((2*i-1)*x) / (2*i-1)
+#     return sum
+
+# x = np.arange(0, 2*np.pi, 0.1)
+# y1 = sin_sum24(x, 50)
+# plt.plot(x, y1)
+# plt.show()
